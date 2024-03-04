dee9e8fcc8aebfd3892490d856251244e8365852
İsmail Türkmen <m.ismailtrkmn@gmail.com>
2024-03-04 15:20:12 +0300

    Add project files.

---
+++ main.js
@@ -0,0 +1,34 @@
+//Header On Scroll Color Change
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+
+});
+
+
+
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    
+}
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay:400,
+})
+
+sr.reveal('.home-text', {origin:'left'})
+sr.reveal('.home-img,.buds-img', {origin:'right'})
+sr.reveal('.heading', {delay:200})
+sr.reveal('.specs-details .box', { origin: 'left', interval: 200 })
+sr.reveal('.specs-img', { delay: 600 })
+sr.reveal('.shop-container .box,.footer .logo,.footer .footer-box', { interval: 150 })
